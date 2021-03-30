f65c2b0786b23b038e30c5840ef0084a5c6641f5
Шматков Иван <shin911611@gmail.com>
2021-03-30 21:51:29 +0300

    add Ceeezar

---
+++ Ceeezar/cmake-build-debug/CMakeFiles/Ceeezar.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ceeezar.dir/main.cpp.obj"
+  "Ceeezar.exe"
+  "Ceeezar.exe.manifest"
+  "Ceeezar.lib"
+  "Ceeezar.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ceeezar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
