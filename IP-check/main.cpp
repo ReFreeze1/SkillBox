73a9fc9496c40d340cc88fa5ae02e2b572be7743
Шматков Иван <shin911611@gmail.com>
2021-03-31 20:19:49 +0300

    Change fails.

--- IP-check/main.cpp
+++ IP-check/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+
+using namespace std;
 //проверяем символы
-bool check_sim(std::string str) {
+bool check_sim(string str) {
     for (int i = 0; i < str.length(); i++) {
         if (str[i] != '.') {
             if (str[i] < '0' || str[i]>'9') {
-                //  std::cout << "\nsim false";
+                //  cout << "\nsim false";
                 return false;
             }
         }
@@ -12,7 +14,7 @@ bool check_sim(std::string str) {
     return true;
 }
 //поиск точек. выдает индекс точки
-int dot_finder(std::string str, int dot) {
+int dot_finder(string str, int dot) {
     int dot_number = 0;
     for (int i = 0; i < str.length(); i++) {
         if (str[i] == '.') {
@@ -23,7 +25,7 @@ int dot_finder(std::string str, int dot) {
     }
 }
 //перевод из char в int
-int convert(std::string str, int dot_number) {
+int convert(string str, int dot_number) {
     int dot_last;
     int dot ;
     dot_number == 1 ? dot_last = -1 : dot_last =dot_finder(str,dot_number-1) ;
@@ -34,33 +36,26 @@ int convert(std::string str, int dot_number) {
         num =((int)str[j] - (int)'0') * k + num ;
         k *= 10;
     }
-    // std::cout << num;
+    // cout << num;
     return num;
 }
 //проверка числа от 0 до 255
 bool check_number(int number) {
-
-    if (number > 255 || number < 0) {
-        //  std::cout << "\nnum false";
-        return false;
-    }
-    else
-        return true ;
-
+    return number > 255 || number < 0;
 }
 //проверяем первые нули
 bool check_zeroLead(std::string str) {
     if (str[0] == '0') return false;
     for (int i = 1; i < str.length(); i++) {
         if ( str[i] !='.'&& str[i - 1] == '0' && str[i+1]!='.'&& i!=(str.length()-1)) {
-            //  std::cout << "\nZERO false";
+            //  cout << "\nZERO false";
             return false;
         }
     }
     return true;
 }
 //считаем точки
-bool dot_counter(std::string str) {
+bool dot_counter(string str) {
     int count = 0;
     for (int i = 0; i < str.length(); i++) {
         if (str[i] == '.') count++;
@@ -69,14 +64,14 @@ bool dot_counter(std::string str) {
     if (count == 3)
         return true;
     else {
-        //  std::cout << "\ndot false";
+        //  cout << "\ndot false";
         return false; }
 }
 //проверяем правильность ip
-bool check_IP(std::string str) {
-    if (!dot_counter(str) || !check_sim(str) || !check_zeroLead(str) || !check_number(convert(str, 1))||
-        !check_number(convert(str, 2)) || !check_number(convert(str, 3))
-        || !check_number(convert(str, 4))) {
+bool check_IP(string str) {
+    if (!dot_counter(str) || !check_sim(str) || !check_zeroLead(str) || check_number(convert(str, 1))||
+        check_number(convert(str, 2)) || check_number(convert(str, 3))
+        || check_number(convert(str, 4))) {
         // std::cout << "\ncheck_IP false";
         return false;
     }
@@ -86,8 +81,8 @@ bool check_IP(std::string str) {
 int main()
 {
     for (;;) {
-        std::string ip;
-        std::cin >> ip;
-        !check_IP(ip) ? std::cout << "\nNo\n" : std::cout << "\nYes\n";
+        string ip;
+        cin >> ip;
+        !check_IP(ip) ? cout << "\nNo\n" : cout << "\nYes\n";
     }
 }
