a2ff17bd64982b7899cc9d2e52460c32dcaf9ea7
Шматков Иван <shin911611@gmail.com>
2021-03-31 21:46:13 +0300

    Fixed mistake.

--- Email/main.cpp
+++ Email/main.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
+#include <string>
+
 
 //делит адресс на 2 части
-int mail_devide(std::string str, int part) {
+std::string  mail_devide(std::string str, int part) {
+std::string temp="";
 
-    int k=0;
-    for (int i = 0; i < str.length(); i++) {
-        if (str[i] == '@') {
-            k = i;
+    if (part == 1)
+    {
+        for(int i=0;i<str.find('@');i++){
+            temp+=str[i];
         }
     }
-    if (part == 1)
-        return k;
-    else if (part == 2)
-        return k + 1;
+    else if (part == 2){
+        for(int i=str.find('@')+1;i<str.length();i++){
+            temp+=str[i];
+        }
+
+    }
+    return temp;
+
 }
 //проверяет на коректность символов
-bool sim_valid(std::string str, int part, int dog) {
+bool sim_valid(std::string str, int part) {
     std::string map1{ ".!#$%&'*+-/=?^_`{|}~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"};
     std::string map2{ "-.abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890" };
     if (part == 1) {
-        for (int i = 0; i < dog; i++) {
+        for (int i = 0; i < str.length(); i++) {
             for (int j = 0; j < map1.length(); j++) {
                 if (str[i] == map1[j])
                     break;
@@ -29,7 +36,7 @@ bool sim_valid(std::string str, int part, int dog) {
         }
     }
     else if (part == 2) {
-        for (int i = dog; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) {
             for (int j = 0; j < map2.length(); j++) {
                 if (str[i] == map2[j])
                     break;
@@ -38,26 +45,22 @@ bool sim_valid(std::string str, int part, int dog) {
             }
         }
     }
+    return true;
 
 }
 //счетчик
-int counter(std::string str, int part) {
-    int count=0;
-    if (part == 1) {
-        for (int i = 0; i < mail_devide(str, part); i++) {
+int counter(const std::string& str) {
+    int count;
+
+        for (int i = 0; i < str.length(); i++) {
             count++;
         }
-    }
-    else if (part == 2) {
-        for (int i = mail_devide(str, part); i <str.length() ; i++) {
-            count++;
-        }
-    }
+
     return count;
 }
 //проверка точек
 bool dot(std::string str) {
-    if (str[0] == '.' || str[mail_devide(str, 2)] == '.') {
+    if (str[0] == '.' ) {
         return false;
     }
     else {
@@ -70,27 +73,42 @@ bool dot(std::string str) {
     return true;
 
 }
+
+bool dog_finder(std::string str){
+    int k=0;
+    for(int i=0; i<str.length();i++){
+        if(str[i]=='@') k++;
+        if(k>1 ||k==0&&i==str.length()-1) return false;
+    }
+    return true;
+}
 //проверка валидности адреса
-bool validation(std::string str) {
-    if (mail_devide(str,1)==0 || !sim_valid(str, 1, mail_devide(str, 1)) || !sim_valid(str, 2, mail_devide(str, 2))
-        || counter(str, 1) > 64 || counter(str, 2) > 63 || !dot(str))
-        return false;
-    else return true;
+bool validation(std::string  const& str) {
+    if (!dog_finder(str)|| !dot(str))return false;
+    else {
+        std::string part1 = mail_devide(str, 1);
+        std::string part2 = mail_devide(str, 2);
+        if (!sim_valid(part1, 1) || !sim_valid(part2, 2)
+            || part1.length() > 64 || part2.length() > 63)
+            return false;
+        else return true;
+    }
+
 }
 
 int main()
 {
-
+for(;;) {
     std::string email;
     std::cin >> email;
 
     if (!validation(email)) {
         std::cout << "\n No\n";
-    }
-    else {
+    } else {
         std::cout << "\n Yes\n";
     }
 
 
+}
 }
 
